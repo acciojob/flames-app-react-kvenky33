ed8924b832da016aefbaef6a200a8f4e8fea3a73
konalam venkatesh <konalamvenkatesh111@gmail.com>
2023-02-20 15:27:13 +0530

    flames

--- src/components/App.js
+++ src/components/App.js
@@ -1,101 +1,69 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-const relation = (str1, str2) => {
-  let common = "";
-  for (let i = 0; i < str1.length; i++) {
-    if (str2.indexOf(str1[i]) !== -1) {
-      common += str1[i];
-      str2 = str2.replace(str1[i], "");
-    }
-  }
-
-  let str1WithoutCommon = "";
-  for (let i = 0; i < str1.length; i++) {
-    if (common.indexOf(str1[i]) === -1) {
-      str1WithoutCommon += str1[i];
-    }
-  }
-
-  let str2WithoutCommon = "";
-  for (let i = 0; i < str2.length; i++) {
-    str2WithoutCommon += str2[i];
-  }
-
-  let totalLength = str1WithoutCommon.length + str2WithoutCommon.length;
-  switch (totalLength % 6) {
-    case 1:
-      return "Friends";
-    case 2:
-      return "Love";
-    case 3:
-      return "Affection";
-    case 4:
-      return "Marriage";
-    case 5:
-      return "Enemy";
-    case 0:
-      return "Siblings";
-    default:
-      return "";
-  }
-};
-
 const App = () => {
-  const [str1, setStr1] = useState("");
-  const [str2, setStr2] = useState("");
-  const [relationType, setRelationType] = useState("");
-
-  const handleStr1Change = (event) => {
-    setStr1(event.target.value);
+  let RelationshipObj = {
+    0: "Siblings",
+    1: "Friends",
+    2: "Love",
+    3: "Affection",
+    4: "Marriage",
+    5: "Enemy",
   };
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+  const [result, setResult] = useState("");
 
-  const handleStr2Change = (event) => {
-    setStr2(event.target.value);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    name === "name1" ? setName1(value) : setName2(value);
   };
-
-  const handleSubmit = () => {
-    // event.preventDefault();
-    if (str1 && str2) {
-      setRelationType(relation(str1, str2));
-    } else {
-      setRelationType("Please Enter valid input");
+  const handleRelation = () => {
+    if (!name1 || !name2) {
+      setResult("Please Enter valid input");
     }
+    let strLength = name1.length + name2.length;
+    for (let i = 0; i < name1.length; i++) {
+      if (name2.includes(name1[i])) {
+        strLength = strLength - 2;
+      }
+      break;
+    }
+    setResult(RelationshipObj[strLength % 6]);
   };
   const handleClear = () => {
-    setStr1("");
-    setStr2("");
-    setRelationType("");
+    setName1("");
+    setName2("");
+    setResult("");
   };
-
   return (
     <div id="main">
-      <label>
-        <input
-          type="text"
-          data-testid="input1"
-          value={str1}
-          onChange={handleStr1Change}
-        />
-      </label>
+      {/* Do not remove the main div */}
 
-      <label>
-        <input
-          type="text"
-          data-testid="input2"
-          value={str2}
-          onChange={handleStr2Change}
-        />
-      </label>
-
-      <button onClick={handleSubmit} data-testid="calculate_relationship">
-        Calculate Relationship further
+      <input
+        type="text"
+        value={name1}
+        name="name1"
+        data-testid="input1"
+        onChange={handleChange}
+        placeholder="Enter first name"
+      />
+      <input
+        type="text"
+        value={name2}
+        name="name2"
+        data-testid="input2"
+        onChange={handleChange}
+        placeholder="Enter second name"
+      />
+      <button data-testid="calculate_relationship" onClick={handleRelation}>
+        Calculate Relationship Future
       </button>
-      <button onClick={handleClear} data-testid="clear">
-        clear
+      <button data-testid="clear" onClick={handleClear}>
+        Clear
       </button>
-
-      <div>{<h3 data-testid="answer">{relationType}</h3>}</div>
+      <br />
+      <h3>{result}</h3>
     </div>
   );
 };
